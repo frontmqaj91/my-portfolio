d7b5eba775762eab3af8f5bc9921f9b4ea956ad5
Mo Abd <mqaj2391@gmail.com>
2026-02-15 09:59:24 -0500

    add background image

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,12 @@ import { useState } from "react";
 export default function Home() {
   const [isOpen, setIsOpen] = useState(false);
   return (
-    <main className="min-h-screen bg-gradient-to-br from-gray-950 via-gray-900 to-black text-white scroll-smooth">
+    <main
+    className="min-h-screen bg-cover bg-center"
+    style={{
+      backgroundImage: "url('/bg.png')"
+    }}
+    >
       <nav className="fixed top-0 left-0 w-full backdrop-blur-md bg-black/30 border-b border-gray-800 z-50">
   <div className="max-w-6xl mx-auto px-6 py-4 flex justify-between items-center">
     
